456c4d80a6351ef413304194d8f778c1318af4b3
Santhoshkumar <santhoshkumarstudy@gmail.com>
2024-09-25 17:28:39 +0530

    changes

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json())
 app.use(cors({
-    origin : "http://localhost:3000"
+    origin : "ecommerce-frontend-mauve-ten.vercel.app" //https://localhost:3000
 }))
 
 const port = 5000;
